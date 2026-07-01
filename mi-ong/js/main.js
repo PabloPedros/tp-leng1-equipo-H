9eee927a2f7bd60bca6045e84e05dc67c8d8da83
Pablo Pedros <pedrospablomau@gmail.com>
2026-07-01 20:31:22 -0300

    Agregado el modo oscuro

---
+++ mi-ong/js/main.js
@@ -0,0 +1,15 @@
+const boton = document.getElementById("btn-modo");
+boton.addEventListener("click", () => {
+ document.body.classList.toggle("oscuro");
+ if(document.body.classList.contains("oscuro")){
+    boton.innerHTML = "☀️";
+}else{
+    boton.innerHTML = "🌙";
+}
+
+})
+
+;
+
+
+
